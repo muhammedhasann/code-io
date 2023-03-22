7d90c72eb12de327abc2a569063fe547c5da7991
Muhammed Hasan <mohammadhasan22003@gmail.com>
2023-03-23 01:24:54 +0300

    p

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = '/'
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
 
 if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+  assetPrefix = `/${github.com/muhammedhasann/code-io}/`;
+  basePath = `/${github.com/muhammedhasann/code-io}`;
 }
 
 module.exports = {
-  assetPrefix: assetPrefix,
+  assetPrefix: assetPrefix',
   basePath: basePath,
-}
+  images: {
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+  },
+};
