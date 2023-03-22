7dc7e436e69531c419e1da4b3eb6b4e0b9bba609
Muhammed Hasan <mohammadhasan22003@gmail.com>
2023-03-23 02:49:45 +0300

    p

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
-/**
-* @type {import('next').NextConfig}
-*/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
   experimental: {
     appDir: false,
   },
 };
 
-export default nextConfig;
-
+module.exports = nextConfig;
