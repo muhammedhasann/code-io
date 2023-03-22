0cf4f854486f2621cf473a4c64916fb5a4c8356d
Muhammed Hasan <mohammadhasan22003@gmail.com>
2023-03-23 02:57:34 +0300

    p

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     appDir: false,
   },
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+
 };
 
 module.exports = nextConfig;
