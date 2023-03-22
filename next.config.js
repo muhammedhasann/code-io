df4cf2fae32bcfe51dc1262fa733150017c726d5
Muhammed Hasan <87802950+muhammedhasann@users.noreply.github.com>
2023-03-23 01:06:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production';
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 module.exports = {
-  assetPrefix: isProd ? 'https://muhammedhasann.github.io/code-io/' : '',
-  images: {
-    unoptimized: true,
-  },
-};
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
